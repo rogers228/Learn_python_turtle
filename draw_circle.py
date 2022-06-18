e931ee66e24f68da8798d48f987ae21d5205edf5
rogers Lu <61651257+rogers228@users.noreply.github.com>
2022-06-18 11:05:24 +0800

    finish draw_semicircle

--- draw_circle.py
+++ draw_circle.py
@@ -1,4 +1,5 @@
 import turtle
+import math
 
 paper = None
 myTurtle = None
@@ -11,7 +12,7 @@ def myfirst():
     global myTurtle
     myTurtle = turtle.Turtle(); t = myTurtle
     t.width(2)
-    t.speed('fastest') # fastest, fast, normal slow, slowest
+    t.speed('fast') # fastest, fast, normal slow, slowest
 
 def myfinished():
     p = paper
@@ -19,22 +20,51 @@ def myfinished():
     t.hideturtle()
     p.exitonclick()
 
-def draw_circle(x, y, r, pen_color = 'black', fill_color = None):
+def draw_semicircle( # 畫半圓
+    x, y, lenght, radius_ratio, rotation = 'r', angle = 0, pen_color = 'black', fill_color = None):
+    '''
+        x, y:       起始點絕對座標
+        lenght:     邊長度
+        radius_ratio 半徑比例 0.1~1 (1等同半徑等同長度的一半)
+        height:     半圓高度
+        height:     半圓高度 與半徑的筆
+        rotation:   畫圓方向 r順時針 l逆時針
+        angle:      起始絕對角度
+        pen_color:  筆顏色
+        fill_color: 填充顏色
+        '''
+    a = lenght/2
+    if radius_ratio < 0.1:
+        radius_ratio = 0.1
+    if radius_ratio > 1:
+        radius_ratio = 1
+    height = radius_ratio*(lenght/2) # 半圓高度
+    r = (a**2 + height**2)/(2*height) # 半徑
+    angle_g= math.degrees(math.acos(a/r)) # 角度
+    angle_all= 2*(90-math.degrees(math.acos(a/r))) # 整體角度
+
     t = myTurtle
     t.pencolor(pen_color)
     if fill_color:
         t.fillcolor(fill_color)
     t.up()
-    t.goto(x, y-r)
+    t.goto(x, y)
     t.down()
-    t.setheading(0) # angle
+    t.setheading(angle) # angle
     t.begin_fill()
-    t.circle(r, 360)
+    t.forward(lenght)
+    if rotation == 'l': # l逆時針
+        t.right(90 + angle_g)
+        t.circle(-r, angle_all)
+    else: # r順時針
+        t.left(90 + angle_g) 
+        t.circle(r, angle_all)
     t.end_fill()
 
 def test1():
     myfirst()
-    draw_circle(0, 0, 100, 'black', 'blue')
+    draw_semicircle(0, 0, 100, 1,'l', 10, 'black', 'yellow')
+    draw_semicircle(-3, 10, 100, 1,'r',10)
     myfinished()
 
 test1()
