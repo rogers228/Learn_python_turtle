4b6bf553c6139c2f745dc910c5ed569eafbe1a40
rogers Lu <61651257+rogers228@users.noreply.github.com>
2022-06-23 10:53:07 +0800

    更新arc

--- draw_arc.py
+++ draw_arc.py
@@ -19,7 +19,7 @@ def myfinished():
     t.hideturtle()
     p.exitonclick()
 
-def draw_arc(x, y, r_min, angle_arc, angle_start, scales, pen_color = 'black'):
+def draw_arc(x, y, r_min, angle_arc, angle_start, scales, pen_color = 'black', fill_color = None):
     # r_min = 10        最小半徑 負值為反向
     # angle_arc = 270   圓弧角 
     # angle_start =120  起始角度 
@@ -32,19 +32,24 @@ def draw_arc(x, y, r_min, angle_arc, angle_start, scales, pen_color = 'black'):
     t.up()
     t.goto(x, y)
     t.setheading(angle_start)
+    if fill_color:
+        t.fillcolor(fill_color)
+        t.begin_fill()
     t.down()
     for s in scales:
         pro = s / sum(scales) # 所占比例 Proportion
         a = angle_arc * pro
         r = r_min*sum(scales) * pro
         t.circle(r, a)
+    t.end_fill()
 
 def test1():
     myfirst()
+    draw_arc(40,-50,15,300,30,[5,4,5],'black','yellow')
     draw_arc(-25,0,10,150,-72,[2,5,2]) # smile 微笑
     draw_arc(25,30,-8,180,90,[1]) # eye 眼睛
     draw_arc(-15,30,-8,180,90,[1]) # eys 眼睛
-    draw_arc(40,-50,15,300,30,[5,4,5])
+    
     myfinished()
 
 test1()
